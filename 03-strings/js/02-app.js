50a69ec3bb415830e5f0f4ec337d0cd398de3674
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-12 23:45:02 +0200

    acabando con los metodos en los strings

--- 03-strings/js/02-app.js
+++ 03-strings/js/02-app.js
@@ -6,5 +6,5 @@ const producto = "monitor 20 Pulgadas";
 // Methods E Includes
 console.log(producto);
 console.log(producto.length); // te dice cuantos caracteres contiene, incluidos los espacios.
-console.log(producto.indexOf('monitor'));  // Busca si la palabra se encuentra en algun lugar de nuestro string, en este caso en la posicion 0;
-
+console.log(producto.indexOf('monitor'));  // Busca si la palabra se encuentra en algun lugar de nuestro string, en este caso en la posicion 0 (-1 es que no existe);
+console.log(producto.includes('monitor')); // Verifica si esta nuestra palabra (true o false);
