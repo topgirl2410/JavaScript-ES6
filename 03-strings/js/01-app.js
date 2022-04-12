3b3352c0dc8adac83dcdf542a6d8ce55faad6510
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-12 23:34:15 +0200

    strings

---
+++ 03-strings/js/01-app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const producto = "Monitor 20 pulgadas";
+const producto2 = String("Monitor 24 pulgadas");
+const producto3 = new String("Monitor 30 pulgadas");
+
+
+
+
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
---
+++ 03-strings/js/02-app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const producto = "monitor 20 Pulgadas";
+
+
+
+
+console.log(producto);
