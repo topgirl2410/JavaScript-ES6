0e4f21d961ebed98476809c53dba5d64f26e7027
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-14 00:05:58 +0200

    operaciones matematicas con javascript

--- 04-numeros/js/01-app.js
+++ 04-numeros/js/01-app.js
@@ -1,1 +1,32 @@
 'use strict';
+
+/*
+Este capítulo presenta los conceptos, objetos y funciones que se utilizan para
+trabajar y realizar cálculos utilizando números y fechas en JavaScript.
+Esto incluye el uso de números escritos en varias bases, incluyendi decimal,
+binario y hexadecimal, así como el uso del objeto global Math para realizar 
+una amplia variedad de operaciones matemáticas con números. */
+
+// Creación de números...
+const numero1 = 20;
+const numero2 = "20";
+
+/****************************************************************************** */
+
+console.log(numero1);
+console.log(numero2);
+
+/******************************************************************************** */
+
+const num1 = 30;
+const num2 = 15;
+const num3 = 20.20;
+const num4 = .102030;
+const num5 = -20;
+
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
---
+++ 04-numeros/js/02-app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const numero1 = 20;
+const numero2 = 35;
+
+
+let resultado;
+
+// Suma
+
+resultado = numero1 + numero2;
+
+// Resta
+
+resultado = numero1 - numero2;
+
+// Multiplicacion
+
+resultado = numero1 * numero2;
+
+// Division
+
+resultado = numero1 / numero2;
+
+// Modulo
+
+resultado = numero1 % numero2;
+
+/*************************************************************************************** */
+
+console.log(resultado);
