02deeee6e13e753234c67082cd61af285ae5c9b3
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-13 23:03:23 +0200

    uppercase, lowercase

---
+++ 03-strings/js/07-app.js
@@ -0,0 +1,18 @@
+'use strict';
+const nombre = "Maria";
+// Convertir strings en mayusculas o minusculas...
+console.log(nombre.length);
+console.log(nombre.toUpperCase());  // Las convierte a mayusculas...
+console.log(nombre.toLowerCase()); // Las convierte a minusculas...
+
+/*************************************************************************************** */
+const email = "correo@correo.com";
+const email2 = "Correo@correo.com";
+
+/****************************************************************************************** */
+console.log(email.toUpperCase());
+console.log(email.toLowerCase());
+
+/****************************************************************************************** */
+console.log(email2.toUpperCase());
+console.log(email2.toLowerCase());
