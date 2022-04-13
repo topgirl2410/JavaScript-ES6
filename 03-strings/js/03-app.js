3ff82ee7778c526ad12c2a5b0f3dfedb17cbfa4d
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-13 21:33:10 +0200

    metodos con strings

--- 03-strings/js/03-app.js
+++ 03-strings/js/03-app.js
@@ -1,7 +1,17 @@
 'use strict';
- const producto = "pantalla plana Xl";
+// Maneras de concatenar Strings
+
+ const producto = "pantalla plana Xl ";
+ const precio = "30 Euros";
 
 
 
 
- console.log(producto);
+ console.log(producto.concat(precio));
+ console.log(producto.concat('En descuento'));
+
+
+ console.log(producto + "con un precio de: " + precio);
+ console.log("El producto " + producto + "tiene un precio de: " + precio);
+
+ console.log(`El producto ${producto} tiene un precio de $ ${precio}`); // Manera más profesional de hacerlo.
