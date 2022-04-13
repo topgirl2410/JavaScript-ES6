9a9ecc3aef00b32b9e27e4e0c85dc6e04932b689
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-13 22:32:13 +0200

    .repeat .split

---
+++ 03-strings/js/06-app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const estudiante = "Maria Pinto ";
+
+  // .repeat() te va a permitir repetir una cadena de texto...
+
+
+  // Si a .repeat() le pasas un numero que no es entero, el lo redondea... (pregunta para entrevista...);
+const texto = "en promoción ".repeat(3);
+
+
+//console.log(texto);
+console.log(`${estudiante} , ${texto}`);
+console.log(estudiante);
+
+// .Split(), te va a permitir dividir un String...
+const clases = "Estoy aprendiendo JavaScript Moderno ES6"
+
