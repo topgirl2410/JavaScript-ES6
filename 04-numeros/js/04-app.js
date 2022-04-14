f3a8256b927e21005f20337c08241b4ed632a5d6
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-14 22:03:23 +0200

    incremento y decrememnto

---
+++ 04-numeros/js/04-app.js
@@ -0,0 +1,16 @@
+'use strict';
+//El orden de las operaciones
+let resultado;
+
+resultado = 20 + 30 * 2;
+
+resultado = (20 + 30) * 2;
+
+// 20% de descuento en tu carrito de compras
+
+resultado = (20 + 30 + 40 + 50 + 60) * .2;
+
+// Impuestos (IVA)
+resultado = (20 + 30) * .21;
+
+console.log(resultado);
---
+++ 04-numeros/js/05-app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Incrementar
+
+let puntuacion = 5;
+
+puntuacion++; // 6
+++puntuacion; // 7
+
+puntuacion += 3; // 10
+
+// Decrementar
+let puntos = 3;
+--puntos; // 2
+puntos--; // 1
+
+
+
+/************************************************************************************ */
+console.log(puntuacion); // INC
+console.log(puntos); // DEC
