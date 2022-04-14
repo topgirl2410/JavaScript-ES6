efbcc3f8589605a8aa648edcd197112eae8df3fc
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-14 23:32:37 +0200

    booleans

---
+++ 05-Operadores/js/03-app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Undefined
+
+let numero;
+
+console.log(numero); // Undefined
+console.log(typeof numero); // Undefined
+
+// Null
+
+let num = null;
+
+console.log(num); // Null
+console.log(typeof num); // Object
+
+/************************************************************************************* */
+
+console.log(numero === num); // False
