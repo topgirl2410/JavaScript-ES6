f7a5c33f5760cc30db23b97a994e9d640e92da14
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-14 22:59:08 +0200

    operadores

---
+++ 05-Operadores/js/01-app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const num1 = 20;
+const num2 = "20";
+const num3 = 30;
+
+
+// Operador mayor a....
+
+console.log(num1 > num3); // False;
+
+// Operador menor a...
+console.log(num1 < num3); // True;
