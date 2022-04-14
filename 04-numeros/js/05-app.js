2f009da5c6a6f8796de7e4283744ee0d67c60538
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-14 22:07:36 +0200

    inc , dec

--- 04-numeros/js/05-app.js
+++ 04-numeros/js/05-app.js
@@ -7,12 +7,13 @@ let puntuacion = 5;
 puntuacion++; // 6
 ++puntuacion; // 7
 
-puntuacion += 3; // 10
+puntuacion += 3; // 10 (suma 3);
 
 // Decrementar
-let puntos = 3;
---puntos; // 2
-puntos--; // 1
+let puntos = 7;
+--puntos; // 6
+puntos--; // 5
+puntos -= 3; // 2 (resta 3);
 
 
 
