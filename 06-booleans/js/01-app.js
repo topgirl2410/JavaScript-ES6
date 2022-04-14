854a60f2c46612bdfa2e3d4cb2e0691f13d1c798
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-14 23:51:39 +0200

    boolean

--- 06-booleans/js/01-app.js
+++ 06-booleans/js/01-app.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const boolean1 = true;
+const boolean2 = false;
+const boolean3 = "true";
+
+
+
+console.log(boolean1);
+console.log(boolean2);
+
+
+console.log(typeof boolean1); // Boolean
+console.log(typeof boolean2); // Boolean
+console.log(typeof boolean3);
+
+console.log(boolean1 == boolean3); // False
+
+
+const boolean4 = new Boolean(true); // Constructor
+console.log(typeof boolean4); // Object
