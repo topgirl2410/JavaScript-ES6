487fee4bb235a89e8efc586b28e380bb263ba8f8
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-15 21:24:56 +0200

    objetos dentro de objetos

---
+++ 07-objetos/js/05-app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Objeto dentro de objeto
+
+const producto = {
+    nombre: "pantalla plana",
+    precio: 250,
+    disponible: true,
+    informacion: {
+        medidas: {
+            peso: "1kg",
+            medida: "1m"
+        },
+        fabricacion: {
+            pais: "España"
+        }
+    }
+}
+
+//console.log(producto);
+console.log(producto);
