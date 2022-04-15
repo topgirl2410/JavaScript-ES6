90b83ff79fd1e3ee628878a9d9f6bd6f8e05952d
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-15 19:11:07 +0200

    objetos

---
+++ 07-objetos/js/01-app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * Un objeto es una coleccion de datos y/o funcionalidad relacionados.
+ * Estos suelen constar de varias variables y funciones (que se denominan
+ * propiedades y métodos cuando estan dentro de objetos).
+ */
+
+
+const nombre = "Monitor 20 pulgadas";
+const precio = 450;
+const disponible = true;
+
+/** Object Literal **/
+const producto = {
+    nombre: "Monitor 20 pulgadas",
+    precio: 450,
+    disponible: true
+}
+
+
+console.log(producto);
