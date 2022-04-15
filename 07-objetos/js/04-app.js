d94394a96b9f46303527b6a30f85e63dbef2a955
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-15 21:02:11 +0200

    destructuring

--- 07-objetos/js/04-app.js
+++ 07-objetos/js/04-app.js
@@ -1,1 +1,18 @@
 'use strict';
+const producto = {
+    nombre: "pantalla plana",
+    precio: 250,
+    disponible: true
+}
+
+// const nombre = producto.nombre; // Forma anterior de hacerlo
+
+// console.log(nombre);
+
+/************************************ Destructuring ************************************************ */
+const { nombre, precio, disponible } = producto;
+
+/************************************************************************************* */
+console.log(nombre);
+console.log(precio);
+console.log(disponible);
