3c7de89546fa9d9d4eb0f712a1873eed4e4ec82d
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-17 23:10:35 +0200

    funcion y métodos

---
+++ 09-funciones/js/04-app.js
@@ -0,0 +1,15 @@
+'use strict';
+const num1 = 20;
+const num2 = "20";
+
+console.log(parseInt(num2)) // Esto es una función
+
+console.log(num1.toString()); // Esto es un método
+
+/********************************************************************************************/
+
+function restar() {
+    console.log(3 - 1)
+}
+
+restar();
