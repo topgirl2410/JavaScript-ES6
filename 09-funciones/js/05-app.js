c3e81202361fc7af7a98f004234e57b81a29bc6f
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-17 23:19:24 +0200

    creando funciones más dinamicas

--- 09-funciones/js/05-app.js
+++ 09-funciones/js/05-app.js
@@ -8,3 +8,12 @@ function sumar(a, b) { // a y b se les conoce como parametros
 sumar(3, 2); // 2 y 3 se les conoce como argumentos
 sumar(200, 500);
 sumar(1200, 2400);
+sumar(528, 796);
+
+/*************************************** Función un poco más dinamica **************************************************/
+
+function saludar(nombre, apellido) {
+    console.log(`Hola soy ${nombre} ${apellido} y vivo en Galicia`);
+}
+
+saludar("María", "Pinto");
