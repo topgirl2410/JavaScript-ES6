2c01dd2ad90a045b6da40f7bb3d782baec10390d
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-17 19:29:48 +0200

    agregar valores al array

---
+++ 08-arrays/js/03-app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto"];
+
+
+
+console.table(meses);
+
+// Tamaño del array
+console.log(meses.length);
---
+++ 08-arrays/js/04-app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto"];
+
+/* Agregar nuevos valores al array */
+
+meses[0] = "nuevo mes"; // Modifica el valor
+
+meses[8] = "septiembre"; // Agrega un nuevo valor 
+
+console.table(meses);
