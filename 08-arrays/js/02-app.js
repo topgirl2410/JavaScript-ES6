fabaff3eed4c7f1aae4d2dcf77ec5f9ca8f8d4a2
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-17 19:06:27 +0200

    acceder a los valores de un array

---
+++ 08-arrays/js/02-app.js
@@ -0,0 +1,10 @@
+'use strict';
+const numeros = [10, 20, 30, 40, 50];
+
+
+console.log(numeros);
+console.table(numeros); // Crea una tabla en la consola
+
+// Acceder a los valores de un array
+console.log(numeros[0]);
+console.log(numeros[3]);
