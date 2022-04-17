6efca33701c53a79ab6772e5771bcb5e24f8e11e
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-17 17:24:55 +0200

    acabando con los objetos, pasamos a arrays

--- 07-objetos/js/13-app.js
+++ 07-objetos/js/13-app.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const producto = {
+    nombre: "ordenador portatil",
+    precio: 1250,
+    disponible: true
+}
+
+console.log(Object.keys(producto)); // Retorna las llaves del producto
+console.log(Object.values(producto)); // Retorna los valores del producto
+console.log(Object.entries(producto)); // Retorna todo el producto
