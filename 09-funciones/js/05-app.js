94acc45cec94fbc70ee210ca66711855071cbad8
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-17 23:14:04 +0200

    parametros y argumentos

--- 09-funciones/js/05-app.js
+++ 09-funciones/js/05-app.js
@@ -1,1 +1,10 @@
 'use strict';
+/** Parametros y Argumentos **/
+function sumar(a, b) { // a y b se les conoce como parametros
+    console.log(a + b)
+}
+
+/************************************************************************************ */
+sumar(3, 2); // 2 y 3 se les conoce como argumentos
+sumar(200, 500);
+sumar(1200, 2400);
