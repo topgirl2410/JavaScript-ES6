ecc03bdd511ec3fe4827dfe0593a2e0ae55c133b
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-17 19:07:55 +0200

    mejorando codigo

--- 08-arrays/js/02-app.js
+++ 08-arrays/js/02-app.js
@@ -1,10 +1,11 @@
 'use strict';
 const numeros = [10, 20, 30, 40, 50];
 
-
+/********************************************************************************************************* */
 console.log(numeros);
-console.table(numeros); // Crea una tabla en la consola
+console.table(numeros); // Crea una tabla en la consola (buen uso para ver las posiciones de index y value)
 
+/********************************************************************************************************** */
 // Acceder a los valores de un array
 console.log(numeros[0]);
 console.log(numeros[3]);
