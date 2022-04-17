a71189f14dc9d5ab3a42b16057bef7535e2053fb
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-17 16:18:08 +0200

    destructuring de objetos anidados

---
+++ 07-objetos/js/06-app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const producto = {
+    nombre: "pantalla plana",
+    precio: 250,
+    disponible: true,
+    informacion: {
+        medidas: {
+            peso: "1kg",
+            medida: "1m"
+        },
+        fabricacion: {
+            pais: "España"
+        }
+    }
+}
+
+// Destructuring de objetos anidados
+
+const { nombre, informacion, informacion: { fabricacion: { pais } } } = producto; // (informacion: { fabricacion: { pais } } destructuring de objetos anidados);
+
+
+
+console.log(nombre);
+console.log(informacion);
+console.log(pais);
