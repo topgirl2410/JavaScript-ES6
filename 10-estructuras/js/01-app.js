f30f37025ff387e746589df9351c383201204f73
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-18 08:20:14 +0200

    if

---
+++ 10-estructuras/js/01-app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * ¿Que es un IF?
+ * - If es una estructura de control utilizada para tomar decisiones. Es un condicional
+ * que sirve para realizar unas u otras operaciones en función de una expresión. Funciona
+ * de la siguiente manera, primero se evalua una expresión, si da resultado positivo se 
+ * realizan las acciones relacionadas con el caso positivo.
+ */
+
+const puntuacion = 1001;
+
+if (puntuacion == 1000) {
+    console.log("si es el mismo");
+} else {
+    console.log("no, no se cumple");
+}
---
+++ 10-estructuras/js/02-app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const puntos = 705;
+const puntuacion = 1000;
+const num = 1000;
+
+/*****************************************************************************************/
+
+if (puntos != 700) {  // (!= diferente a...);
+    console.log("si, es diferente");
+} else {
+    console.log("es igual");
+}
+/*****************************************************************************************/
+
+if (puntuacion == "1000") {   // (== igual a...));
+    console.log("si,es igual");
+} else {
+    console.log("es diferente");
+}
+
+/******************************************************************************************/
+
+if (num === "1000") {  // (es exacto)
+    console.log("si es exacto")
+} else {
+    console.log("es diferente...")
+}
