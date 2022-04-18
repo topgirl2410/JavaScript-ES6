66003c44700ff27bc807dcef24cbe80145bc22a9
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-18 18:48:13 +0200

    operador OR

--- 10-estructuras/js/07-app.js
+++ 10-estructuras/js/07-app.js
@@ -1,4 +1,18 @@
 'use strict';
 
-// Operador OR ( revisa que al menos se cumpla una condición )
+// Operador OR ( revisa que al menos se cumpla una condición ), codigo comun en la validacion de formularios;
+
+const efectivo = 300;
+const credito = 400;
+
+const disponible = efectivo + credito;
+const totalAPagar = 600;
+
+
+if (efectivo > totalAPagar || credito > totalAPagar || disponible > totalAPagar) {
+    console.log("Si, podemos cargar la cuenta");
+} else {
+    console.log("Fondos Insuficientes");
+}
+
 
