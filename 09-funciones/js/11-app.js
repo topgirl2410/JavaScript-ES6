eb049e58801b58bc6441c73f0bc57b3ed6e65f1c
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-18 06:18:32 +0200

    jugando con el codigo, probando diferentes maneras con las funciones

--- 09-funciones/js/11-app.js
+++ 09-funciones/js/11-app.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const aprendiendo = function (tecnologia, tecnologia1) {
+    console.log(`Aprendiendo ${tecnologia} y ${tecnologia1}`)
+}
+
+aprendiendo("JavaScript ES6", "Node.js");
+
+/************************************************************ Arrow Function ************************** */
+
+const leer = libros => `Leyendo ${libros} `; // Si solo hay un parametro se puede no usar parentesis, codigo más corto y limpio;
+
+console.log(leer("La Bella y la Bestia"));
+
