13757e70564a0c2135571f75a7359ca345a53bc2
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-18 22:15:57 +0200

    ahora sacar los impares

--- 11-iteradores/js/01-app.js
+++ 11-iteradores/js/01-app.js
@@ -5,10 +5,12 @@
 //console.log(`Numero: ${i}`)
 //};
 
-/******************************************************** Sacar los números pares del 0 al 20 **********************************/
+/******************************************************** Sacar los números pares del 0 al 20 ( Segunda parte: sacar tambien los números impares)**********************************/
 
 for (let i = 1; i <= 20; i++) {
     if (i % 2 === 0) {
-        console.log(`El número ${i} es PAR`)
+        console.log(`El número ${i} es PAR`)  // Saca solo los números pares;
+    } else {
+        console.log(`El número ${i} es IMPAR`)  // Saca solo los números impares;
     }
 }
