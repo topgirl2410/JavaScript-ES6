91214adcb367f8f3de6bdd809269b880684c006a
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-18 19:47:15 +0200

    detener la ejecucion de un IF

--- 10-estructuras/js/08-app.js
+++ 10-estructuras/js/08-app.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const autentificado = true;
+
+if (autentificado) {
+    console.log("El usuario esta registrado");
+}
+/***************************************** Detener la ejecución de un IF ************************************************** */
+const puntos = 500;
+
+
+function revisarPuntuacion() {
+    if (puntos > 400) {
+        console.log("Excelente!!");
+        return;
+    }
+    if (puntos > 300) {
+        console.log("Buena puntuacion...¡¡¡ Felicidades !!!");
+        return;
+    }
+}
+
+revisarPuntuacion();
