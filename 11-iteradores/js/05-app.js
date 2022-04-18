1e687489e9bdd2ceadaa2ae5f7a359850f402027
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-18 23:38:59 +0200

    bucle Do While

---
+++ 11-iteradores/js/05-app.js
@@ -0,0 +1,11 @@
+'use strict';
+// do while se ejecutara al menos una vez, pero siempre se ejecutara (no importa si se cumple o no la condicion)
+
+let i = 0; // Inicializar 
+
+do {
+
+    console.log(`Número ${i}`)
+
+    i++;  // Incremento
+} while (i < 10);  // Condición 
