984dfaf1afa3623514d7d5719abfc204c90009d0
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-18 04:21:25 +0200

    como se comunican las funciones

---
+++ 09-funciones/js/07-app.js
@@ -0,0 +1,14 @@
+'use strict';
+iniciarApp();
+
+function iniciarApp() {
+    console.log("Iniciando app....")
+
+    segundaFuncion();
+}
+
+function segundaFuncion() {
+    console.log("desde la segunda función");
+}
+
+/******************************************************************************************* */
