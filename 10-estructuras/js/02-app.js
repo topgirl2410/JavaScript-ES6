4f61cd6c2b858d575deff6e206d1a9a870d38c04
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-18 08:29:01 +0200

    ejercicios con if y comparadores logicos

--- 10-estructuras/js/02-app.js
+++ 10-estructuras/js/02-app.js
@@ -3,17 +3,19 @@
 const puntos = 705;
 const puntuacion = 1000;
 const num = 1000;
+const num2 = 3000;
+
 
 /*****************************************************************************************/
 
-if (puntos != 700) {  // (!= diferente a...);
+if (puntos != 700) {  // (!= diferente a...) no es un operador estricto;
     console.log("si, es diferente");
 } else {
     console.log("es igual");
 }
 /*****************************************************************************************/
 
-if (puntuacion == "1000") {   // (== igual a...));
+if (puntuacion == "1000") {   // (== igual a... ) no es un operador estricto;
     console.log("si,es igual");
 } else {
     console.log("es diferente");
@@ -21,8 +23,15 @@ if (puntuacion == "1000") {   // (== igual a...));
 
 /******************************************************************************************/
 
-if (num === "1000") {  // (es exacto)
+if (num === "1000") {  // ( === es un comparador estricto )
     console.log("si es exacto")
 } else {
     console.log("es diferente...")
+}
+/******************************************************************************************/
+
+if (num2 !== "1000") {   // ( !== es un comparador estricto )
+    console.log("Si, es diferente");
+} else {
+    console.log("No, no es igual");
 }
