059f6de81f24211bb879da19d0e2f9441028415a
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-18 23:16:17 +0200

    buen ejercicio jajaja fizzbuzz

--- 11-iteradores/js/02-app.js
+++ 11-iteradores/js/02-app.js
@@ -1,2 +1,39 @@
 'use strict';
 
+/******************************************** Break *******************************************************/
+/*for (let i = 0; i <= 10; i++) {
+    if (i === 5) {
+        console.log("Este es el 5");
+        break; // Se detiene la ejecución del For Loop (Rompe, detiene la ejecucion)
+    }
+
+    console.log(`Número: ${i}`);
+}*/
+/************************************************ Continue **************************************************/
+/*for (let i = 0; i <= 10; i++) {
+    if (i === 5) {
+        console.log("Cinco");
+        continue; // Continua con el siguiente número (en este caso con el seis)(rompe el ciclo, pero continua)
+    }
+
+    console.log(`Numero: ${i}`);
+}*/
+
+/**************************************************** Descuento en la tienda *********************************************************/
+
+const carrito = [
+    { nombre: "Monitor 20 pulgadas", precio: 300 },
+    { nombre: "Laptop", precio: 400 },
+    { nombre: "Altavoces", precio: 100 },
+    { nombre: "Portatil", precio: 1200, descuento: true },
+    { nombre: "Teclado", precio: 151 },
+    { nombre: "Smarphone", precio: 1400 },
+]
+
+for (let i = 0; i < carrito.length; i++) {
+    if (carrito[i].descuento) {
+        console.log(`El articulo ${carrito[i].nombre} Tiene un descuento`);
+        continue;
+    }
+    console.log(carrito[i].nombre)
+}
---
+++ 11-iteradores/js/03-app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/******************************** Ejercicio Fizz Buzz ( 100 Números ) **************************************************************/
+
+// 3, 6, 9, 12 (multiplos de 3) imprimimos Fizz
+
+// 5, 10, 15, 20 (multiplos de 5) imprimimos Buzz
+
+// 15, 30, 45, 60 imprimimos FizzBuzz!!
+
+
+/**********************************************************************************************************************/
+
+for (let i = 1; i < 100; i++) {
+    if (i % 15 === 0) {
+        console.log(`${i} FizzBuzz!!`)
+    } else if (i % 3 === 0) {
+        console.log(`${i} fizz`)
+    } else if(i % 5 === 0) {
+        console.log(`${i} buzz`)
+    }
+}
