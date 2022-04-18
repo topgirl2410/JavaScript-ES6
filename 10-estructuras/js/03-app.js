46da9fd8fcab829e55a305628836a6e4421b9fa3
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-18 08:41:28 +0200

    operador  mayor que, menor que

---
+++ 10-estructuras/js/03-app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**************************************(Operador mayor que...)*********************************************** */
+
+const money = 500;
+const gastos = 300;
+
+
+if (money > gastos) {
+    console.log("Tarjeta aceptada")
+} else {
+    console.log("Error en el pago")
+}
+
+/*********************************************(Operador menor que...)********************************* */
+const dinero = 200;
+const totalAPagar = 450;
+
+if (dinero < totalAPagar) {
+    console.log("Error en el pago")
+} else {
+    console.log("Tarjeta aceptada")
+}
