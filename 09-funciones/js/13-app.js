67b0cee3b0b322dee6c674fe59f9761ded4bf2b0
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-18 07:25:47 +0200

    agregando carpeta de estructuras

--- 09-funciones/js/13-app.js
+++ 09-funciones/js/13-app.js
@@ -2,13 +2,26 @@
 /**************************** Reproductor de música con Arrow Function *************************************************************** */
 
 const reproductor = {
+    cancion: '',
     reproducir: id => console.log(`Reproduciendo canción con el id ${id}`),
     pausar: () => console.log(`Pausando...`),
     borrar: id => console.log(`Borrando canción... ${id}`),
     crearPlaylist: nombre => console.log(`Creando una playlist de ${nombre}`),
-    reproducirPlaylist: nombre => console.log(`Reproduciendo la playlist de ${nombre}`)
+    reproducirPlaylist: nombre => console.log(`Reproduciendo la playlist de ${nombre}`),
+
+    set nuevaCancion(cancion) {
+        this.cancion = cancion;
+        console.log(`Añadiendo ${cancion}`)
+    },
+    get obtenerCancion() {
+        console.log(`${this.cancion}`)
+    }
 }
 /************************************************************************************************************* */
+reproductor.nuevaCancion = "Enter Sandman"; // Set
+reproductor.obtenerCancion; // Get
+
+
 
 reproductor.reproducir(15);
 reproductor.reproducir(35);
@@ -17,3 +30,4 @@ reproductor.borrar(35);
 reproductor.crearPlaylist("Bachata");
 reproductor.crearPlaylist("Rock 80s");
 reproductor.reproducirPlaylist("Rock 80s");
+
