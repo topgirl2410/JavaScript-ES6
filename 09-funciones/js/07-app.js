0881338db7cc05a5b3a2afeff154738ebd35531f
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-18 04:28:18 +0200

    comunicación entre funciones

--- 09-funciones/js/07-app.js
+++ 09-funciones/js/07-app.js
@@ -5,10 +5,18 @@ function iniciarApp() {
     console.log("Iniciando app....")
 
     segundaFuncion();
+
 }
+/********************************************************************************************** */
 
 function segundaFuncion() {
     console.log("desde la segunda función");
+    usuarioAutentificado("María");
 }
 
 /******************************************************************************************* */
+
+function usuarioAutentificado(usuario) {
+    console.log("Autentificando usuario...comprobando...");
+    console.log(`Usuario Autentificado correctamente: ${usuario}`);
+}
