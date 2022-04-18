27d096c4f3ef62404687af68ce8201c5bd318088
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-18 05:20:04 +0200

    metodos de propiedad

--- 09-funciones/js/09-app.js
+++ 09-funciones/js/09-app.js
@@ -1,1 +1,37 @@
 'use strict';
+// Metodos de propiedad
+
+const reproductor = {
+    reproducir: function (id) {
+        console.log(`Reproduciendo canción con el id ${id}`)
+    },
+    siguiente: function () {
+        console.log(`pasando a la siguiente canción...`)
+    },
+    crear: function () {
+        console.log(`Creando tu playList`);
+    },
+    playlist: function () {
+        console.log(`Reproduciendo tu playlist`)
+    },
+    pausar: function () {
+
+        console.log(`pausando reproductor...`)
+    },
+
+    parar: function () {
+        console.log(`Parando el reproductor de música...`)
+    },
+    borrar: function (id) {
+        console.log(`Borrando listado de canciones... ${id}`)
+    }
+}
+
+/****************************************************************************************** */
+reproductor.reproducir(30);
+reproductor.siguiente();
+reproductor.crear();
+reproductor.playlist();
+reproductor.pausar();
+reproductor.parar();
+reproductor.borrar(30);
