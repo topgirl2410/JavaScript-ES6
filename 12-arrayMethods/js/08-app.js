b34722717d34fd6069f6746ccde9c1912abd692a
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-20 01:08:50 +0200

    array methods acabado, proximo JS DOM

--- 12-arrayMethods/js/08-app.js
+++ 12-arrayMethods/js/08-app.js
@@ -2,14 +2,15 @@
 
 const meses = ["Enero", "Febrero", "Marzo", "Abril"];
 
-// Ejercicio: Agrega el mes de mayo al final de meses
-const meses1 = [...meses, "Mayo"]; // No modifica el arreglo original (buena practica)
+// Spread Operator con arregle de indices ( Ejercicio: Agrega el mes de mayo al 5q, m de meses )
+
+const meses1 = [...meses, "Mayo"]; // No modifica el arreglo original (buena practica programación funcional)
 
 
 console.log(meses); //El Arreglo Original se matiene intacto
 console.log(meses1);
 
-/************************************************************************************************** */
+/***************************************************** Ejercicio 2 (Spread Operator) **********************************************/
 
 const carrito = [
     { nombre: "Monitor", precio: 180 },
@@ -18,3 +19,13 @@ const carrito = [
     { nombre: "Altavoces", precio: 500 },
     { nombre: "Xiaomi 11T PRO", precio: 799 }
 ]
+
+// Agrega un producto al arreglo de carrito 
+
+const producto = { nombre: "Disco Duro", precio: 300 }
+
+const carrito1 = [...carrito, producto];
+
+
+console.log(carrito) // Arreglo Original
+console.log(carrito1)   
