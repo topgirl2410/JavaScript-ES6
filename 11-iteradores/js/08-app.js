d2f10fa8d7ef10cc22db42407c885bc54cf19e04
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-19 12:42:44 +0200

    for in

---
+++ 11-iteradores/js/08-app.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const casa = ["tender", "lavar", "fregar", "cocinar", "planchar"];
+
+/*************************************** For In********************************************** */
