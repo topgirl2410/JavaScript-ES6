3913ebd60436cf4b6bfb7f4f33ecb481d073b756
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-19 18:24:55 +0200

    findIndex

--- 12-arrayMethods/js/02-app.js
+++ 12-arrayMethods/js/02-app.js
@@ -4,9 +4,9 @@ const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio"];
 
 const carro = [
     { nombre: "Monitor 20 pulgadas", precio: 250 },
-    { nombre: "Televisión", precio: 350 },
+    { nombre: "Televisión", precio: 100 },
     { nombre: "Tablet", precio: 289 },
-    { nombre: "Auriculares", precio: 89 },
+    { nombre: "Auriculares", precio: 100 },
     { nombre: "Teclado", precio: 75 },
     { nombre: "Smartphone", precio: 899 }
 ];
@@ -26,3 +26,13 @@ meses.forEach((mes, i) => {
 const indice = meses.findIndex(mes => mes === "Abril");
 
 console.log(indice);
+
+/*************************************************** Ejercicio ************************************************************* */
+
+// Encontrar el indice de un arreglo de objetos....
+
+const indice1 = carro.findIndex(producto => producto.precio === 100); // Solo te retorna el primero que encuentra;
+
+
+
+console.log(indice1);
