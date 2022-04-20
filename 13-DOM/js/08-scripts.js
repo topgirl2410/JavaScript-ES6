544beb82ad67b7fdfea0e42ba786b78512bd6bf6
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-21 00:38:59 +0200

    acabando trasversing the DOM

--- 13-DOM/js/08-scripts.js
+++ 13-DOM/js/08-scripts.js
@@ -10,20 +10,20 @@ console.log(navegacion.children); // Te va a listar elementos reales;
 console.log(navegacion.children[0].nodeName);
 console.log(navegacion.children[0].nodeType);
 
-/*************************************************************************************************************/
+/*******************************************************************************************************************************************************************************************/
 
 const card = document.querySelector('.card');
 
 console.log(card.children); // Nos lista los elementos que sean hijos de ese card
 console.log(card.children[1].children);
 
-/************************************* Acceder al titulo y al texto ************************************ */
+/************************************* Acceder al titulo y al texto *************************************************************************************************************************/
 
 const card1 = document.querySelector('.card');
 
 console.log(card1.children[1].children[1].textContent);
 
-/************************************* Modificar el titulo *************************************************/
+/************************************* Modificar el titulo **********************************************************************************************************************************/
 
 const card2 = document.querySelector('.card');
 
@@ -31,7 +31,7 @@ card2.children[1].children[1].textContent = 'Nuevo heading desde traversing the 
 
 console.log(card2.children[1].children[1].textContent);
 
-/************************************* Modificar la imagen *************************************************/
+/************************************* Modificar la imagen *********************************************************************************************************************************/
 
 const card3 = document.querySelector('.card');
 
@@ -39,3 +39,29 @@ card3.children[0].src = 'img/hacer3.jpg';
 
 console.log(card3.children[0]);
 
+/***************************************** Traversing del hijo al padre *******************************************************************************************************************/
+
+const tarjeta = document.querySelector('.card');
+
+console.log(tarjeta.parentNode); // Toma en cuenta los espacios en blanco 
+
+console.log(tarjeta.parentElement.parentElement.parentElement); // Verifica que los elementos son validos (ira hasta el main en este caso) (es la forma más segura de hacerlo)
+
+/************************************************************* Seleccionar elementos *****************************************************************************************************************************/
+
+const target = document.querySelector('.card');
+
+console.log(target);
+
+console.log(target.nextElementSibling);
+
+console.log(target.nextElementSibling.nextElementSibling);
+
+
+/*****************************************************************************************************************************************************************************************************************/
+
+const ultimoCard = document.querySelector('.card:nth-child(4)');
+
+console.log(ultimoCard);
+
+console.log(ultimoCard.previousElementSibling); // Recorre hacia el elemento previo (hacia atras)
