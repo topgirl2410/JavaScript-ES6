2b314d0d142812d0c7f91dc49fa841d42dfaef39
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-20 22:53:23 +0200

    agregar clases al DOM

---
+++ 13-DOM/js/07-scripts.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Cambiar el color del encabezado con JavaScript (No se recomienda)
+
+const encabezado = document.querySelector('h1');
+
+encabezado.style.backgroundColor = 'red';
+
+// Cambiar la fuente del encabezado
+
+encabezado.style.fontFamily = 'Arial';
+
+// Poner el texto en mayusculas 
+
+encabezado.style.textTransform = 'uppercase';
+
+/********************************** Agregar clases ****************************************************/
+
+const card = document.querySelector('.card');
+card.classList.add('nueva-clase');
+console.log(card.classList);
+
+
+/************************************* Agregar una segunda clase ***************************************/
+const card1 = document.querySelector('.card');
+card.classList.add('nueva-clase1', 'segunda-clase');
+console.log(card1.classList);
