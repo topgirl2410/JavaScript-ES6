8815c3d3734a8004d37876be50778c4b2dce7b39
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-20 23:29:58 +0200

    traversing the DOM

---
+++ 13-DOM/js/08-scripts.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/*************************************** Traversing the DOM ***********************************************/
+
+const navegacion = document.querySelector('.navegacion');
+
+console.log(navegacion);
+console.log(navegacion.childNodes);  // Los espacios en blanco (saltos de linea) son considerados elementos
+
+
