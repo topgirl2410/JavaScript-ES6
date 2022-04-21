86249bc4b27dfd715d4e5b2a0134a743673c2d86
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-21 19:49:58 +0200

    eventos mouse

---
+++ 14-eventos/js/02-app.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const nav = document.querySelector('.navegacion');
+
+// Registrar un evento
+
+/*nav.addEventListener('click', () => {
+    console.log('click en nav')
+});*/
+
+/************************************ Eventos (mouseenter) ***********************************************************/
+
+/*nav.addEventListener('mouseenter', () => {  // se activa pasando el mouse por encima del enlace
+    console.log('entrando a la navegación')
+
+    nav.style.backgroundColor = 'white';
+});*/
+
+/************************************ Eventos (mouseout) ***********************************************************/
+
+/*nav.addEventListener('mouseout', () => {   // Se activa cuando sales de encima de la navegación
+    console.log('saliendo de la navegación')
+
+    nav.style.backgroundColor = 'transparent';
+})*/
+
+/************************************ Eventos (mousedown) ***********************************************************/
+
+/*nav.addEventListener('mousedown', () => {
+    console.log('presionando la navegación')
+})*/
+
+
+/************************************ Eventos (mouseup) ***********************************************************/
+
+nav.addEventListener('mouseup', () => {  // se activa apretando y soltando el enlace
+    console.log('nave')
+})
+
+/************************************ Eventos (dblclick) ***********************************************************/
+
+nav.addEventListener('dblclick', () => {  // Se activa dandole doble click
+    console.log('nav')
+})
+
+
+/***
+ * *************************** Cuando se activan los eventos *****************************************************
+ * 
+ * - mousedown = similar al click
+ * 
+ * - click = cuando das click
+ * 
+ * - dblclick = haciendo doble click
+ * 
+ * - mouseup = cuando sueltas el mouse 
+ * 
+ * 
+ */
