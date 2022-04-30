d174c14c956f46e80bbd9f85a9d4e0bc9b08b220
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-30 22:28:16 +0200

    JSON.strigify

--- 15-localStorage-ES6/js/01-app.js
+++ 15-localStorage-ES6/js/01-app.js
@@ -7,3 +7,22 @@ localStorage.setItem('nombre', 'Maria');
 // Session Storage
 
 sessionStorage.setItem('apellido', 'Pinto');
+
+/********************************************************************************************* */
+const producto = {
+    nombre: "monitor 20 pulgadas",
+    precio: 250
+}
+
+const productoString = JSON.stringify(producto);
+
+localStorage.setItem('producto', productoString);
+
+/********************************************************************************************** */
+
+const dias = ["Lunes", "Martes", "Miercoles", "Jueves",
+    "Viernes", "Sabado", "Domingo"];
+
+const diasString = JSON.stringify(dias);
+
+localStorage.setItem('semana', diasString);
