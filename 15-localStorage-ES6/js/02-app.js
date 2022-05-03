4ed265c38fc82b4618bc463ffb3f26641c081503
topgirl2410 <maria.topgirl82@gmail.com>
2022-05-03 15:26:02 +0200

    obtener datos de local storage

---
+++ 15-localStorage-ES6/js/02-app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Obtener datos de LocalStorage
+
+const nombre = localStorage.getItem('nombre');
+console.log(nombre);
+
+
+const productoJSON = localStorage.getItem('producto');
+console.log(JSON.parse(productoJSON));
+
+const diasJSON = localStorage.getItem('semana');
+console.log(JSON.parse(diasJSON));
