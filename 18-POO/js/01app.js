839ca3166084814d3b7b13692f60d166a39f360a
topgirl2410 <maria.topgirl82@gmail.com>
2022-05-11 20:17:26 +0200

    definiendo una clase

---
+++ 18-POO/js/01app.js
@@ -0,0 +1,24 @@
+'use strict';
+/************************************* Class  Declaration ***************************************/
+
+class Cliente {
+    constructor(nombre, saldo) {
+        this.nombre = nombre;
+        this.saldo = saldo;
+    }
+}
+
+const nombre = new Cliente('Marco', 400);
+console.log(nombre);
+
+/*************************************** Class Expresion (apenas se utiliza) ******************************************** */
+
+const Cliente2 = class {
+    constructor(nombre, saldo) {
+        this.nombre = nombre;
+        this.saldo = saldo;
+    }
+}
+
+const nombre2 = new Cliente2('Maria', 800);
+console.log(nombre2)
