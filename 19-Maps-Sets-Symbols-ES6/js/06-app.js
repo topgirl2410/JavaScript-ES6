f4bc420433a96bc60e6a64ee55e30b3638c4cdb4
topgirl2410 <maria.topgirl82@gmail.com>
2022-05-20 22:48:51 +0200

    iteradores

--- 19-Maps-Sets-Symbols-ES6/js/06-app.js
+++ 19-Maps-Sets-Symbols-ES6/js/06-app.js
@@ -1,5 +1,5 @@
 'use strict';
-const carrito = ['Producto 1', 'Producto 2', 'Producto 3', 'Producto 4'];
+const carrito = ['Producto 1', 'Producto 2', 'Producto 3', 'Producto 4', 'Producto nuevo'];
 
 function crearIterador(carrito) {
     let i = 0;
@@ -21,6 +21,7 @@ function crearIterador(carrito) {
 // Utilizar el iterador 
 const recorrerCarrito = crearIterador(carrito);
 
+console.log(recorrerCarrito.siguiente());
 console.log(recorrerCarrito.siguiente());
 console.log(recorrerCarrito.siguiente());
 console.log(recorrerCarrito.siguiente());
