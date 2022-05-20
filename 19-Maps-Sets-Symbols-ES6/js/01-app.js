d0652c9cba7b7b92103ededf8c0b52b804c0eb5b
topgirl2410 <maria.topgirl82@gmail.com>
2022-05-20 18:22:35 +0200

    Sets

---
+++ 19-Maps-Sets-Symbols-ES6/js/01-app.js
@@ -0,0 +1,36 @@
+'use strict';
+/**
+ * Sets:
+ * Como en el caso de los arrays tradicionales, un Set de datos es una coleccion de 
+ * valores unicos en JavaScript, sean estos de cualquier tipo: primitivas(String, Number, Boolean
+ * , Symbols) u objetos (referencias);
+ */
+
+/***
+ * Métodos básicos:
+ * add: añade un elemento al Set;
+ * delete: elimina un elemento del Set;
+ * has: Revisa si un valor existe; (true, false);
+ * forEach: permite aplicar una función sobre cada elemento del Set(segun su orden de inserción);
+ */
+
+// Para declarar una nueva instancia 
+const carrito = new Set();
+
+
+carrito.add('Camisa');
+carrito.add('Pantalon');
+carrito.add('Jersey');
+carrito.add('Chaleco');
+
+
+carrito.forEach((producto, index, pertenece) => {
+    console.log(pertenece);
+})
+
+/*************************************************************************************************** */
+console.log(carrito.has('Camisa')); // Revisa si un valor existe
+console.log(carrito.size); // Tamaño 
+//console.log(carrito.delete('Jersey'));  // Elimina
+//console.log(carrito.clear());
+console.log(carrito);
