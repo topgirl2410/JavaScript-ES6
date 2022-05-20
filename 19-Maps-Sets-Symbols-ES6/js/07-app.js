74e02bbbf26e4f9dc71fd4a5c9ea29f5f81b460c
topgirl2410 <maria.topgirl82@gmail.com>
2022-05-20 23:38:56 +0200

    generadores en js

--- 19-Maps-Sets-Symbols-ES6/js/07-app.js
+++ 19-Maps-Sets-Symbols-ES6/js/07-app.js
@@ -12,11 +12,30 @@ function *crearGenerador() {
 
 const iterador = crearGenerador();
 
-console.log(iterador);
-console.log(iterador.next()); // Despierta el generador y despues lo vuelve a "dormir" 
-console.log(iterador.next().value);
-console.log(iterador.next());
-console.log(iterador.next().value);
-console.log(iterador.next());
-console.log(iterador);
+//console.log(iterador);
+//console.log(iterador.next()); // Despierta el generador y despues lo vuelve a "dormir" 
+//console.log(iterador.next().value);
+//console.log(iterador.next());
+//console.log(iterador.next().value);
+//console.log(iterador.next());
+//console.log(iterador);
 
+
+
+// Generador para carrito de compras
+function *generadorCarrito(carrito) {
+    for(let i = 0; i < carrito.length; i++) {
+        yield carrito[i]
+    }
+}
+
+
+const carrito = ['producto1', 'producto2', 'producto3', 'producto4'];
+const iterador1 = generadorCarrito(carrito);
+
+
+console.log(iterador1.next());
+console.log(iterador1.next());
+console.log(iterador1.next());
+console.log(iterador1.next());
+console.log(iterador1.next());
