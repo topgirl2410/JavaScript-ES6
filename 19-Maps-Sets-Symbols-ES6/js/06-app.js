01dcd13dd559dcca9019673f583842f0f4389e4e
topgirl2410 <maria.topgirl82@gmail.com>
2022-05-20 22:46:51 +0200

    creando un iterador

---
+++ 19-Maps-Sets-Symbols-ES6/js/06-app.js
@@ -0,0 +1,28 @@
+'use strict';
+const carrito = ['Producto 1', 'Producto 2', 'Producto 3', 'Producto 4'];
+
+function crearIterador(carrito) {
+    let i = 0;
+
+    return {
+        siguiente: () => {
+
+            const fin = (i >= carrito.length);
+            const valor = !fin ? carrito[i++] : undefined;
+
+            return {
+                fin,
+                valor
+            }
+        }
+    }
+}
+
+// Utilizar el iterador 
+const recorrerCarrito = crearIterador(carrito);
+
+console.log(recorrerCarrito.siguiente());
+console.log(recorrerCarrito.siguiente());
+console.log(recorrerCarrito.siguiente());
+console.log(recorrerCarrito.siguiente());
+console.log(recorrerCarrito.siguiente());
