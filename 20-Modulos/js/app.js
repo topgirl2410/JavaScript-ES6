b5d0c61cd62f2ccc542eea99d595c10b55f5bb7d
topgirl2410 <maria.topgirl82@gmail.com>
2022-05-22 00:38:19 +0200

    exportar e imprtar una clase

--- 20-Modulos/js/app.js
+++ 20-Modulos/js/app.js
@@ -1,5 +1,13 @@
-import { nombre, ahorro, mostrarInformacion } from './cliente.js';
+import { nombre, ahorro, mostrarInformacion, tieneSald, Cliente } from './cliente.js';
 
 console.log(nombre);
 console.log(ahorro);
 console.log(mostrarInformacion(nombre, ahorro));
+
+
+tieneSald(ahorro); // Funcion
+
+const cliente = new Cliente(nombre, ahorro);  // Class
+
+
+console.log(cliente.mostrarInformacion());
--- 20-Modulos/js/cliente.js
+++ 20-Modulos/js/cliente.js
@@ -1,9 +1,27 @@
 export const nombre = 'maria';
 export const ahorro = 200;
 
-/*************************************************************************************************************************** */
+/*************************************************** Importar Funciones ************************************************************************ */
 export function mostrarInformacion(nombre, ahorro) {
     return `Cliente: ${nombre} - Ahorro: ${ahorro}`;
 }
 
-mostrarInformacion();
+export function tieneSald(ahorro) {
+    if (ahorro > 0) {
+        console.log('Si tiene saldo')
+    } else {
+        console.log('Te has quedado sin saldo, recarga')
+    }
+}
+
+/******************************************************* Importar Clases ************************************************************************ */
+export class Cliente {
+    constructor(nombre, ahorro) {
+        this.nombre = nombre;
+        this.ahorro = ahorro;
+    }
+
+    mostrarInformacion() {
+        return `Cliente: ${nombre} - Ahorro: ${ahorro}`
+    }
+}
