1c1749d803f96b8f1e1bed618f2f091cbadf168a
topgirl2410 <maria.topgirl82@gmail.com>
2022-05-22 00:58:47 +0200

    heredar una clase que esta siendo importada

--- 20-Modulos/js/app.js
+++ 20-Modulos/js/app.js
@@ -11,3 +11,11 @@ const cliente = new Cliente(nombre, ahorro);  // Class
 
 
 console.log(cliente.mostrarInformacion());
+
+// Importar empresa 
+import { Empresa } from './empresa.js';
+
+const empresa = new Empresa('Aprende con Maria', 510, 'JavaScript');
+
+
+console.log(empresa.mostrarInformacion());
