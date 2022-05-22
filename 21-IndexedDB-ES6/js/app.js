13a24d004cb457f2b1c8df02b81832631ffb70b8
topgirl2410 <maria.topgirl82@gmail.com>
2022-05-22 16:38:18 +0200

    creando las tablas

--- 21-IndexedDB-ES6/js/app.js
+++ 21-IndexedDB-ES6/js/app.js
@@ -20,8 +20,21 @@ function crmDB() {
     }
 
     // Configuración de la base de datos
-    crmDB.onupgradeneeded = function () {
-        console.log('Este método solo se ejecuta una vez');
+    crmDB.onupgradeneeded = function (e) {
+        const db = e.target.result;
+
+        const objectStore = db.createObjectStore('crm', {
+            keyPath: 'crm',
+            autoIncrement: true
+        });
+
+        // Definir las columnas 
+        objectStore.createIndex('nombre', 'nombre', { unique: false });
+        objectStore.createIndex('email', 'email', { unique: true });
+        objectStore.createIndex('telefono', 'telefono', { unique: false });
+
+
+        console.log('Columnas creadas');
     }
 
 }
